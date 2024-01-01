7f334875687c3c8212279afa84b4e28aee20ffa0
MINSEONG <54202716+okmin06@users.noreply.github.com>
2024-01-01 17:41:46 +0900

    Update push.js

--- push.js
+++ push.js
@@ -7,14 +7,14 @@ const commands = [
     }
 ];
 
-const rest = new REST({ version: '10' }).setToken("MTE4NjIzNzEzMDQ4MjEyMjc5Mg.GIXxHw.1K_k4Y7Rz2llsdwQAZr36U3jYtQSTqfjlnGZXY");//bot token
+const rest = new REST({ version: '10' }).setToken("");//bot token
 
 (async () => {
     try {
         console.log('Started refreshing application (/) commands.');
 
         await rest.put(
-            Routes.applicationCommands("1186237130482122792", "1186174856908247081"),//client_id, guild_id
+            Routes.applicationCommands("", ""),//client_id, guild_id
             { body: commands },
         ) 
             console.log('Successfully reloaded application (/) commands.');
